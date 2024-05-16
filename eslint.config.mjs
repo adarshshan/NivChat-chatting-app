9c0ed420928f6bdf82802a32164ad2255b87fa01
Adarsh <adarshshanu3@gmail.com>
2024-05-16 16:50:00 +0530

    add a package too

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,7 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    "extends": ["@babel/plugin-transform-private-property-in-object", "next/core-web-vitals"]
+  }
 ];
